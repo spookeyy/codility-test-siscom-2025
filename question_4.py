a0c477328fb05e5bb4dd2181cb3fa3e03b77d3be
gokkuu100 <wkurts247@gmail.com>
2025-07-31 11:47:29 +0300

    question4_5

---
+++ question_4.py
@@ -0,0 +1,25 @@
+"""
+Overview
+
+You need to help Bob be a good businessman and not charge people too much for his signs.
+Description
+
+Bob is running a business that creates signs for people. He can charge much less than his competitors because he charges by letter instead by the entire sign. He can take a sign and change a few letters to make a new sign much more cheaply than a competitor can make a sign from scratch.
+
+The only problem is Bob is not very good at pricing these changes. He wants to be able to look at a sign and a customer's request and quickly be able to give the customer an estimate for the total cost.
+Task
+
+Define a function estimate(add_cost, remove_cost, old_sign, new_sign) -> minimum_cost that is adaptable to changes in the market, and can help Bob estimate prices quickly.
+
+The first 2 arguments are the costs of doing an operation, of adding and removing a letter respectively.
+The last 2 arguments are the old sign of the customer, and their request.
+
+It should return the cost of changing the sign from the old message to the new message. If there are multiple ways to change the sign, it should return the cheapest way.
+
+
+
+"""
+
+
+def estimate(add_cost, remove_cost, old_sign, new_sign):
+    return 0
