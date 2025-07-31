9c5e25dc44ff9bd2efca452852df3276b3d93cbd
spookeyy <pangasmeshack@gmail.com>
2025-07-31 13:48:23 +0300

    returning the longest palindrome from the sting "s"

--- question_2.py
+++ question_2.py
@@ -30,4 +30,16 @@ def longestPalindrome(s):
         :type s: str
         :rtype: str
         """
-        
+        #return longestPlalindrome from the string "s"
+        longestP = ""
+        for i in range(len(s)):
+            for j in range(i + 1, len(s) + 1):
+                sub = s[i:j]
+                if sub == sub[::-1] and len(sub) > len(longestP):
+                    longestP = sub
+        return print("This is the longest palindromestring: ",longestP) if len(longestP) > 0 else longestP
+
+longestPalindrome("babad")
+longestPalindrome("dgoohddil")
+longestPalindrome("cbbd")
+longestPalindrome("tactoaa")
