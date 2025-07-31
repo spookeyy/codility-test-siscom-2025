94fae8d635fc6f8532282a105c5c032f07bc3837
spookeyy <pangasmeshack@gmail.com>
2025-07-31 13:57:49 +0300

    not yet done

--- question_3.py
+++ question_3.py
@@ -56,6 +56,18 @@ s and words[i] consist of lowercase English letters.
 
 "#Solution"
 
+#Decalre a map to store the words
+#Decalre a list to store the indices
+#Decalre a variable to store the word length
+# traverse through all possible substrings in "s" and check if it is a concatenation of all words
+# if it is, add the starting index to the list
+
+# create a temporary map and initialize it with original map to store every possible substring
+# extract from the words from the substring and if the words is not present in the temporary map, break,
+# else if it is present, we decrese its corresponding count
+
+# after traversing the substing, we traverse temp map for any word whose count is not 0, if it is, we add the starting index to the result
+
 
 def findSubstring(s, words):
         """
@@ -63,4 +75,57 @@ def findSubstring(s, words):
         :type words: List[str]
         :rtype: List[int]
         """
+        # size of word in words
+        size_of_word = len(words[0])
+
+        count_of_words = len(words)
+        print("word count is ", count_of_words)
+
+        # total chars present in word
+        total_wordSize = size_of_word * count_of_words
+
+        results = []
+
+        # if total num of chars in words is greater than that of string "s"
+        if total_wordSize > len(s):
+                return results
+        
+        # Map stores the words present in words
+        # against it's occurrences inside "words"
+        hash_map = dict()
+
+        for i in range(count_of_words):
+                if words[i] in hash_map:
+                        hash_map[words[i]] += 1
+                else:
+                        hash_map[words[i]] = 1
+
+        for i in range(0, len(s) - total_wordSize + 1, 1):
+                temp_hash_map = hash_map.copy()
+                j = i
+                count = count_of_words
+
+                # traversing the substring
+                while j < i + total_wordSize:
+                        # word extraction
+                        word = s[j:j + size_of_word]
+                        if word in temp_hash_map:
+                                temp_hash_map[word] -= 1
+                                if temp_hash_map[word] == 0:
+                                        count -= 1
+                        else:
+                                break
+                        j += size_of_word
+
+                # Store the starting index of that substring
+                # when all the words in the list are in substring
+                if count == 0:
+                        results.append(i)
+
+                return print("Outpur is ", results)
+
+
+# testing the function
+findSubstring("barfoothefoobarman", ["foo","bar"])
+findSubstring("barfoofoobarthefoobarman", ["bar","foo","the"])
         
